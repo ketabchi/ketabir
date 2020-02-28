a98245893990b879f4ea9bd1f86286f0805a2c56
Mohammad Zolfaghari <zidom72@gmail.com>
2020-02-28 21:21:09 +0330

    Separate client for each request
    ketab.ir uses cookie to persist search data. This is causing mix
    in results on concurrent requests. IMHO copying client and using
    separate client for each GetBookURLByISBN call will solve this.

--- api/client.go
+++ api/client.go
@@ -14,6 +14,13 @@ import (
 	"github.com/antzucaro/matchr"
 )
 
+type urlFinder struct {
+	isbn   string
+	title  string
+	client http.Client
+	doc    *goquery.Document
+}
+
 var client = &http.Client{}
 
 func SetClient(c *http.Client) {
@@ -27,7 +34,12 @@ func GetBookURLByISBN(isbn string, args ...string) (string, error) {
 		return "", nil
 	}
 
-	body, err := createPostBodyISBN(isbn)
+	uf := &urlFinder{isbn: isbn, client: *client}
+	if len(args) > 0 {
+		uf.title = args[0]
+	}
+
+	body, err := uf.createBody()
 	if err != nil {
 		return "", err
 	}
@@ -38,7 +50,7 @@ func GetBookURLByISBN(isbn string, args ...string) (string, error) {
 		return "", err
 	}
 
-	res, err := client.Do(req)
+	res, err := uf.client.Do(req)
 	if err != nil {
 		return "", err
 	}
@@ -47,55 +59,21 @@ func GetBookURLByISBN(isbn string, args ...string) (string, error) {
 		return "", fmt.Errorf("not 200 on sending request")
 	}
 
-	doc, err := goquery.NewDocumentFromResponse(res)
+	uf.doc, err = goquery.NewDocumentFromResponse(res)
 	if err != nil {
 		return "", err
 	}
 
-	link, chap := "", 0
-	el := doc.Find("#ctl00_ContentPlaceHolder1_DataList1 > tbody > tr")
-	el.EachWithBreak(func(i int, sel *goquery.Selection) bool {
-		s := sel.Find("tr:nth-child(2) span").Text()
-		ss := chapRe.FindStringSubmatch(s)
-		if len(ss) < 2 {
-			return true
-		}
-
-		pass := true
-		if len(args) > 0 {
-			title := sel.Find(".HyperLink2").Text()
-			title = strings.TrimSpace(title)
-			tmp := matchr.SmithWaterman(args[0], title)
-			tmp /= float64(len([]rune(args[0])))
-
-			if tmp <= 0.2 {
-				pass = false
-			}
-		}
-
-		if pass {
-			ch, _ := strconv.Atoi(ss[1])
-			if ch > chap {
-				chap = ch
-				href, _ := sel.Find(".HyperLink2").Attr("href")
-
-				link = fmt.Sprintf("http://ketab.ir%s", href)
-			}
-		}
-
-		return true
-	})
-
-	return link, nil
+	return uf.find(), nil
 }
 
-func createPostBodyISBN(isbn string) (io.Reader, error) {
-	res, err := client.Get("http://ketab.ir/Search.aspx")
+func (uf *urlFinder) createBody() (io.Reader, error) {
+	res, err := uf.client.Get("http://ketab.ir/Search.aspx")
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
-	if err = setClientCookies(res.Cookies()); err != nil {
+	if err = uf.setCookies(res.Cookies()); err != nil {
 		return nil, err
 	}
 
@@ -114,7 +92,7 @@ func createPostBodyISBN(isbn string) (io.Reader, error) {
 	data.Set("__EVENTVALIDATION", v3)
 	data.Set("ctl00$SiteMenu$Search$DropDownFieldList", "1")
 	data.Set("ctl00$SiteMenu$Search$DDLTypeSearch", "1")
-	data.Set("ctl00$ContentPlaceHolder1$TxtIsbn", isbn)
+	data.Set("ctl00$ContentPlaceHolder1$TxtIsbn", uf.isbn)
 	data.Set("ctl00$ContentPlaceHolder1$drpDewey", "-1")
 	data.Set("ctl00$ContentPlaceHolder1$drpFromIssueYear", "57")
 	data.Set("ctl00$ContentPlaceHolder1$drpFromIssueMonth", "01")
@@ -130,7 +108,7 @@ func createPostBodyISBN(isbn string) (io.Reader, error) {
 	return strings.NewReader(data.Encode()), nil
 }
 
-func setClientCookies(cs []*http.Cookie) error {
+func (uf *urlFinder) setCookies(cs []*http.Cookie) error {
 	u, err := url.Parse("http://ketab.ir")
 	if err != nil {
 		return err
@@ -142,7 +120,44 @@ func setClientCookies(cs []*http.Cookie) error {
 	}
 
 	cj.SetCookies(u, cs)
-	client.Jar = cj
+	uf.client.Jar = cj
 
 	return nil
+}
+
+func (uf *urlFinder) find() (link string) {
+	chap := 0
+	el := uf.doc.Find("#ctl00_ContentPlaceHolder1_DataList1 > tbody > tr")
+	el.EachWithBreak(func(i int, sel *goquery.Selection) bool {
+		s := sel.Find("tr:nth-child(2) span").Text()
+		ss := chapRe.FindStringSubmatch(s)
+		if len(ss) < 2 {
+			return true
+		}
+
+		pass := true
+		if uf.title != "" {
+			title := sel.Find(".HyperLink2").Text()
+			title = strings.TrimSpace(title)
+			tmp := matchr.SmithWaterman(uf.title, title)
+			tmp /= float64(len([]rune(uf.title)))
+
+			if tmp <= 0.2 {
+				pass = false
+			}
+		}
+
+		if pass {
+			ch, _ := strconv.Atoi(ss[1])
+			if ch > chap {
+				chap = ch
+				href, _ := sel.Find(".HyperLink2").Attr("href")
+
+				link = fmt.Sprintf("http://ketab.ir%s", href)
+			}
+		}
+
+		return true
+	})
+	return
 }
